93f8d06f79a065600349fef9914199c8deec693d
Anastasiya Efimova <asya@yandex.ru>
2023-07-02 19:57:47 +0300

    new_commit

--- tests.py
+++ tests.py
@@ -22,3 +22,59 @@ class TestBooksCollector:
 
     # напиши свои тесты ниже
     # чтобы тесты были независимыми в каждом из них создавай отдельный экземпляр класса BooksCollector()
+
+
+    def test_add_new_book_add_two_books_doubles(self):
+        collector = BooksCollector()
+        collector.add_new_book('Гордость и предубеждение и зомби')
+        collector.add_new_book('Гордость и предубеждение и зомби')
+        assert len(collector.get_books_rating()) == 1
+    def test_set_book_rating_check_book_rating_5(self):
+        collector = BooksCollector()
+        collector.add_new_book('Гордость и предубеждение и зомби')
+        collector.set_book_rating('Гордость и предубеждение и зомби', 5)
+        assert collector.get_book_rating('Гордость и предубеждение и зомби') == 5
+
+    def test_set_book_rating_check_book_rating_below_the_border(self):
+        collector = BooksCollector()
+        collector.add_new_book('Гордость и предубеждение и зомби')
+        collector.set_book_rating('Гордость и предубеждение и зомби', 0)
+        assert collector.get_book_rating('Гордость и предубеждение и зомби') == 1
+
+    def test_set_book_rating_check_book_rating_above_the_border(self):
+        collector = BooksCollector()
+        collector.add_new_book('Гордость и предубеждение и зомби')
+        collector.set_book_rating('Гордость и предубеждение и зомби', 11)
+        assert collector.get_book_rating('Гордость и предубеждение и зомби') == 1
+
+    def test_get_books_with_specific_rating_5(self):
+        collector = BooksCollector()
+        collector.add_new_book('Гордость и предубеждение и зомби')
+        collector.set_book_rating('Гордость и предубеждение и зомби', 5)
+        collector.add_new_book('Обломов')
+        collector.set_book_rating('Обломов', 10)
+        assert collector.get_books_with_specific_rating(5) == ['Гордость и предубеждение и зомби']
+
+    def test_add_book_in_favorites(self):
+        collector = BooksCollector()
+        collector.add_new_book('Гордость и предубеждение и зомби')
+        collector.add_book_in_favorites('Гордость и предубеждение и зомби')
+        assert len(collector.get_list_of_favorites_books()) == 1
+
+    def test_delete_book_from_favorites(self):
+        collector = BooksCollector()
+        collector.add_new_book('Гордость и предубеждение и зомби')
+        collector.add_book_in_favorites('Гордость и предубеждение и зомби')
+        collector.delete_book_from_favorites('Гордость и предубеждение и зомби')
+        assert len(collector.get_list_of_favorites_books()) == 0
+
+    def test_get_list_of_favorites_books(self):
+        collector = BooksCollector()
+        collector.add_new_book('Гордость и предубеждение и зомби')
+        collector.add_book_in_favorites('Гордость и предубеждение и зомби')
+        assert collector.get_list_of_favorites_books()[0] == 'Гордость и предубеждение и зомби'
+
+
+
+
+
